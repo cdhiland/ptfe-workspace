b79615b6a543918ad2d9fe1a64170b39024c1856
cdhiland <62705982+cdhiland@users.noreply.github.com>
2020-03-26 11:06:52 -0400

    Update main.tf

--- app-vm-dev/main.tf
+++ app-vm-dev/main.tf
@@ -1,7 +1,7 @@
 # Provider Credentials can be loaded via
-# export VSPHERE_SERVER=""
-# export VSPHERE_USER=""
-# export VSPHERE_PASSWORD=""
+variable VSPHERE_SERVER = ""
+variable VSPHERE_USER = ""
+variable VSPHERE_PASSWORD= ""
 provider "vsphere" {
   allow_unverified_ssl = true
 }
